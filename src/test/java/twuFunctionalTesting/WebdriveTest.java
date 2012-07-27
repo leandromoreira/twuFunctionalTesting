9992e04cfee2f5af628e4adf3d0d917667f7cd02
Liziane <liziane.brandini@gmail.com>
2012-07-27 19:01:00 -0300

    Added HTML unit driver, created test to check link in home

--- src/test/java/twuFunctionalTesting/WebdriveTest.java
+++ src/test/java/twuFunctionalTesting/WebdriveTest.java
@@ -5,16 +5,19 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class WebdriveTest {
 
 	
 	@Test
-	public void shouldOpenAppPageWithSelenium(){
-		WebDriver firefox = new FirefoxDriver();
-		firefox.get("http://localhost:8080/twuFunctionalTesting/");
-	
-		
+	public void shouldSeePlaceAnOrderInHome(){
+		WebDriver browser = new HtmlUnitDriver();
+		browser.get("http://localhost:8080/twuFunctionalTesting/");
+
+		String sourcePage = browser.getPageSource();
+		assertTrue(sourcePage.contains("Place an Order"));	
 	}
 	
 }
