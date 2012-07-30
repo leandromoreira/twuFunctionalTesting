cee3e78dff6dcef8424de5bf9a0c2eae6be7fd18
Liziane <liziane.brandini@gmail.com>
2012-07-30 14:21:55 -0300

    Selenium updated and test for placing an order created

--- src/test/java/twuFunctionalTesting/WebdriveTest.java
+++ src/test/java/twuFunctionalTesting/WebdriveTest.java
@@ -3,21 +3,36 @@ package twuFunctionalTesting;
 import java.util.concurrent.TimeUnit;
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.By;
+
 
 public class WebdriveTest {
 
-	
-	@Test
-	public void shouldSeePlaceAnOrderInHome(){
-		WebDriver browser = new HtmlUnitDriver();
-		browser.get("http://localhost:8080/twuFunctionalTesting/");
 
+	@Test
+	public void shouldPlaceAnOrder(){
+		WebDriver browser = new FirefoxDriver();
+		browser.get("http://localhost:8080/twuFunctionalTesting/");
+		browser.findElement(By.partialLinkText("Place an Order")).click();
+
+		WebElement name = browser.findElement(By.id("name_field"));
+		name.sendKeys("Batman");
+
+		WebElement email = browser.findElement(By.id("email_field"));
+		email.sendKeys("batman@gotham.com");
+
+		WebElement combo = browser.findElement(By.id("items"));
+		browser.findElement(By.id("submitButton")).click();
+		
 		String sourcePage = browser.getPageSource();
-		assertTrue(sourcePage.contains("Place an Order"));	
+		browser.quit();
+		assertTrue(sourcePage.contains("Order Saved!"));
 	}
-	
+
 }
